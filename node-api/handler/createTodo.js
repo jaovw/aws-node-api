e9a8e9f467cbf14eb1ad63c0d12198be1df0435e
jaovw <joojvw@gmail.com>
2022-09-23 11:05:43 -0300

    async function

--- node-api/handler/createTodo.js
+++ node-api/handler/createTodo.js
@@ -1,45 +1,36 @@
-const AWS = require("aws-sdk");
-// import { AWS } from 'aws-sdk'
+const { DynamoDB } = require("aws-sdk");
+
 const TODO_TABLE = process.env.TODO_TABLE;
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const dynamoDb = new DynamoDB.DocumentClient();
 const uuid = require('uuid')
 
-module.exports.createTodo = (event, context, callback) => {
+module.exports.createTodo = async (event) => {
 
     const timestamp = new Date().getTime()
     const data = JSON.parse(event.body)
+  
+    const newItem = {
+        userId: uuid.v1(),
+        todo: data.todo,
+        checked: false,
+        createdAt: timestamp,
+        updatedAt: timestamp
+    }
 
     if(typeof data.todo !== 'string') {
         console.error('Validacao falha')
         return
     }
 
-    const params = {
-
+    await dynamoDb.put({
         TableName: TODO_TABLE,
-        Item: {
-            userId: uuid.v1(),
-            todo: data.todo,
-            checked: false,
-            createdAt: timestamp,
-            updatedAt: timestamp
-        }
+        Item: newItem
+    }).promise()
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify(newItem)
     }
-
-
-    dynamoDb.put(params, (err, data) => {
-        if(err) {
-            console.log(err)
-            callback(new Error(err))
-        }
-        const response = {
-
-            statusCode: 200,
-            body: JSON.stringify(data.Item)
-        }
-
-        callback(null, response)
-    })
     
    
 }
