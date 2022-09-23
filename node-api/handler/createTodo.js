8fc3fb8fa79fce5a30f3ba1eae65b91d84142c78
jaovw <joojvw@gmail.com>
2022-09-22 21:56:49 -0300

    rota post com callback

--- node-api/handler/createTodo.js
+++ node-api/handler/createTodo.js
@@ -1,15 +1,45 @@
 const AWS = require("aws-sdk");
 // import { AWS } from 'aws-sdk'
-
 const TODO_TABLE = process.env.TODO_TABLE;
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid')
 
-exports.createTodo = async (event, context) => {
+module.exports.createTodo = (event, context, callback) => {
 
-    return{
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Hello World!'
-        })
+    const timestamp = new Date().getTime()
+    const data = JSON.parse(event.body)
+
+    if(typeof data.todo !== 'string') {
+        console.error('Validacao falha')
+        return
     }
+
+    const params = {
+
+        TableName: TODO_TABLE,
+        Item: {
+            userId: uuid.v1(),
+            todo: data.todo,
+            checked: false,
+            createdAt: timestamp,
+            updatedAt: timestamp
+        }
+    }
+
+
+    dynamoDb.put(params, (err, data) => {
+        if(err) {
+            console.log(err)
+            callback(new Error(err))
+        }
+        const response = {
+
+            statusCode: 200,
+            body: JSON.stringify(data.Item)
+        }
+
+        callback(null, response)
+    })
+    
+   
 }
